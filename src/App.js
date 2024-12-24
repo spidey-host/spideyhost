f10d278d2be51bc1d706be7c90241662211963cf
0xtheM7 <openmyaddress@gmail.com>
2024-12-24 13:29:46 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
@@ -8,13 +8,13 @@ import {
 import Navbar from "./components/Navbar";
 import Footer from './components/Footer.jsx';
 
-import About from "./pages/About.jsx"
+import About from "./pages/About.jsx";
 import Home from "./pages/index.jsx";
-import Shared from './pages/Shared-hosting.jsx'
-import Cloud from './pages/Cloud-hosting.jsx'
-import VPS from './pages/VPS-hosting.jsx'
-import Wordpress from './pages/Wordpress-hosting.jsx'
-import DomainPricing from './pages/DomainPricing.jsx'
+import Shared from './pages/Shared-hosting.jsx';
+import Cloud from './pages/Cloud-hosting.jsx';
+import VPS from './pages/VPS-hosting.jsx';
+import Wordpress from './pages/Wordpress-hosting.jsx';
+import DomainPricing from './pages/DomainPricing.jsx';
 
 function App() {
   return (
@@ -22,35 +22,15 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-                {/* <Route exact path="/" element={<Home />} />
-                <Route path="/about" element={<About />} />
-                <Route
-                    path="/contact"
-                    element={<Contact />}
-                />
-                <Route path="/blogs" element={<Blogs />} />
-                <Route
-                    path="/sign-up"
-                    element={<SignUp />}
-                /> */}
                 <Route exact path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/shared-hosting" element={<Shared/>} />
-                <Route path="/cloud-hosting" element={<Cloud/>} />
-                <Route path="/vps-hosting" element={<VPS/>} />
-                <Route path="/wordpress-hosting" element={<Wordpress/>} />
-                <Route path="/domain" element={<DomainPricing/>} />
-
-
+                <Route path="/shared-hosting" element={<Shared />} />
+                <Route path="/cloud-hosting" element={<Cloud />} />
+                <Route path="/vps-hosting" element={<VPS />} />
+                <Route path="/wordpress-hosting" element={<Wordpress />} />
+                <Route path="/domain" element={<DomainPricing />} />
             </Routes>
       </Router>
-      {/* <Hero />
-      <Clients />
-      <HostingSection />
-      <HostingPackages />
-      <HostingComparison />
-      <FeaturePerformance />
-      <Footer /> */}
       <Footer />
     </div>
   );
